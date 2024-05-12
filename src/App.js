0fba1d75b74e3b40df88d10493c5164ae0364e35
Prashnt Sachan <swepra04@gmail.com>
2024-05-12 16:31:29 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoadMoreData from './components/index';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreData />
+    </div>
+  );
+}
+
+export default App;
